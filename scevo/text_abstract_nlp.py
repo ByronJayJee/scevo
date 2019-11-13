e95d27f337a628f9f1c5cf9292dcdb189f4706b5
ByronJayJee <ByronJayJee@gmail.com>
2019-11-12 20:15:55 -0500

    create demo to do term frequency-inverse document frequency analysis on abstracts

--- scevo/text_abstract_nlp.py
+++ scevo/text_abstract_nlp.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
+import time
+
 from random import random
 
 import json
@@ -11,8 +13,13 @@ import sys
 
 import xml.etree.ElementTree as ET
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 def pub_xml_abstract_grab(pmid=23456789, abstract_dict={}):
+
+
+    time.sleep(0.5)
 
     pmid_str = str(pmid)
 
@@ -26,17 +33,34 @@ def pub_xml_abstract_grab(pmid=23456789, abstract_dict={}):
 
     response = requests.get(req_url)
 
-    root = ET.fromstring(response.text)
+    print('\nPMID %d All attributes:' % (pmid))
+    temp_res_str = response.text.encode("utf-8")
+    #print(temp_res_str)
+    #print(response.text)
+
+    #root = ET.fromstring(response.text)
+    root = ET.fromstring(temp_res_str)
 
     # all item attributes
-    print('\nAll attributes:')
     for elem in root:
         abstract_str = ''
         #abstract_text=elem.findall("MedlineCitation/Article/Abstract/AbstractText")
         abstract_text_elem=elem.findall("MedlineCitation/Article/Abstract/AbstractText")
-        #print(abstract_text_elem)
+        print(abstract_text_elem)
         for res in abstract_text_elem:
-            abstract_str = abstract_str + ' ' + res.text
+            temp_res_str2 = str(ET.tostring(res))
+            '''
+            print('\ntemp_res_str2')
+            print(temp_res_str2)
+            print('\ntemp_res_str2.encode("utf-8")')
+            print(temp_res_str2.encode("utf-8"))
+            '''
+            #print('\nres')
+            #print(res)
+            #print('\nres.text.encode("utf-8")')
+            #print(res.text.encode("utf-8"))
+            #abstract_str = abstract_str + ' ' + res.text
+            abstract_str = abstract_str + ' ' + str(temp_res_str2.encode("utf-8"))
             #print(res.text)
         '''
         print('\n')
@@ -70,3 +94,28 @@ if __name__ == "__main__":
         abstract_dict = pub_xml_abstract_grab(root_pmid, abstract_dict)
 
     print(abstract_dict)
+
+    dictlist = []
+
+    for key, value in abstract_dict.items():
+        temp = [key,value]
+        #dictlist.append(temp)
+        temp_str = ''.join(str(value))
+        dictlist.append(temp_str)
+
+    print(dictlist)
+
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
+    tfidf_matrix =  tf.fit_transform(dictlist)
+
+    feature_names = tf.get_feature_names()
+    print(len(feature_names))
+
+    print(feature_names[50:70])
+
+    dense = tfidf_matrix.todense()
+
+    temp_arr = dense[0].tolist()[0]
+    print(temp_arr)
+    print(len(temp_arr))
+
