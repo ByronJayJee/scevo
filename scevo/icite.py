8d6d77936b4c29178f680bd5a9db1fb70d1d2eb9
Benjamin Clos <benjamin.clos@gmail.com>
2019-11-13 19:10:55 -0500

    feat: add option for --verbose and write to stdout

--- scevo/icite.py
+++ scevo/icite.py
@@ -5,8 +5,8 @@
   - Importable for use by other scripts
 
 Usage:
-  icite.py fetch <pmid> [--out=<out>]
-  icite.py tree <pmid> [--max-depth=<max_depth> | --out=<out>]
+  icite.py fetch <pmid> [--out=<out> | --verbose]
+  icite.py tree <pmid> [--max-depth=<max_depth> | --out=<out> | --verbose]
   icite.py (-h | --help)
   icite.py --version
 
@@ -15,14 +15,18 @@ Options:
   --version                 Show version.
   --max-depth=<max_depth>   Max depth to recurse a publication citation tree [default: 5].
   --out=<out>               The file to dump the json results to [default: icite.json].
+  --verbose
 
 """
-from docopt import docopt
+
+import asyncio
 import types
 import re
+import sys
 import itertools
 import requests
 import json
+import pprint
 
 class Icite:
   #
@@ -36,30 +40,36 @@ class Icite:
   BATCH_KEY  = "data"
   # the key where a publications citation tree lives
   CITED_BY   = "cited_by"
- 
-  def __init__(self):
-    # Network Cache handles:
-    # 1. cyclical references in the Graph
-    # 2. in-memory caching for the entire session
-    self.cached = {}
+  # Network Cache handles:
+  # 1. cyclical references in the Graph
+  # 2. in-memory caching for the entire session
+  CACHE      = {}
 
-  def fetch(self, url, param):
+  @staticmethod
+  def log(stuff):
+    if ("--verbose" in str(sys.argv)):
+      print(stuff)
+  
+  @staticmethod
+  def req(url, param):
     assert isinstance(url, str), "url must be a string: %r" % url
     assert isinstance(param, str), "param must be a string: %r" % param
     prepared_url = url.replace(":param", param)
-    print(":fetch %r" % prepared_url)
+    Icite.log(":req %r" % prepared_url)
     resp = requests.get(prepared_url)
     # todo: handle non 200 responses
     return resp.json()
 
-  def prepare_ids(self, ids = []):
+  @staticmethod
+  def prepare_ids(ids = []):
     assert len(ids) > 0, "cannot prepare empty ids"
     string_ids  = map(lambda id: str(id), ids)
-    needs_fetch = filter(lambda id: id not in self.cached, string_ids)
+    needs_fetch = filter(lambda id: id not in Icite.CACHE, string_ids)
     return ",".join(needs_fetch)
 
   # todo: this could be refactored to be a multithreaded divide-and-conquer algorithm
-  def ids(self, ids = [], fetched = []):
+  @staticmethod
+  def fetch_by_ids(ids = [], fetched = []):
     if len(ids) == 0: return fetched
     # icite api is limited to 100 ids per batch
     # so we slice this first batch
@@ -67,42 +77,52 @@ class Icite:
     # and store the next window to keep recursively fetching
     # as necessary
     next_batch    = ids[100:-1]
-    newly_fetched = self.fetch(Icite.BATCH_URL, param = self.prepare_ids(this_batch))
+    newly_fetched = Icite.req(Icite.BATCH_URL, param = Icite.prepare_ids(this_batch))
     pubs          = newly_fetched[Icite.BATCH_KEY]
-    for pub in pubs: self.cached.update({pub["pmid"]: pub})
-    return self.ids(next_batch, fetched + pubs)
+    for pub in pubs: Icite.CACHE.update({pub["pmid"]: pub})
+    return Icite.fetch_by_ids(next_batch, fetched + pubs)
 
-  def id(self, id):
-    return self.ids([id])[0]
+  @staticmethod
+  def fetch_by_id(id):
+    return Icite.fetch_by_ids([id])[0]
 
   # used to lazily reconstruct a tree task to keep memory complexity down
-  def load(self, ids, table = {}):
+  @staticmethod
+  def backfill(ids, table = {}):
     for id in ids:
-      if id in self.cached: table.update({id: self.cached[id]})
+      if id in Icite.CACHE: table.update({id: Icite.CACHE[id]})
     return table
 
-  def tree(self, ids=[], fetched=[], depth=0, max_depth= 5):
+  @staticmethod
+  def fetch_citation_tree(ids=[], fetched=[], depth=0, max_depth= 5):
     # maybe a dead branch on the tree
-    if len(ids) == 0: return self.load(fetched)
+    if len(ids) == 0: return Icite.backfill(fetched)
     # fetch all ids at this depth
-    pubs = self.ids(ids)
+    pubs = Icite.fetch_by_ids(ids)
     fetched = fetched + ids
-    if depth >= max_depth: return self.load(fetched)
+    if depth >= max_depth: return Icite.backfill(fetched)
     pubs_citations = map(lambda resp: resp[Icite.CITED_BY], pubs)
     next_level = list(itertools.chain.from_iterable(pubs_citations))
-    return self.tree(ids=next_level, fetched=fetched, depth=depth + 1, max_depth=max_depth)
+    return Icite.fetch_citation_tree(ids=next_level, fetched=fetched, depth=depth + 1, max_depth=max_depth)
 
 # currently this loads the test id pub tree to a depth of 5 in ~2.3m
 if __name__ == "__main__":
+  from docopt import docopt
   argv = docopt(__doc__, version='NIH Icite 1.0')
   pmid = [argv["<pmid>"]]
-  icite = Icite()
   fetched = None
   # handle all operations cases
   if argv["fetch"]: 
-    fetched = icite.ids(pmid)
+    fetched = Icite.fetch_by_ids(pmid)
   if argv["tree"]:  
-    fetched = icite.tree(ids=pmid, max_depth=int(argv["--max-depth"]))
+    fetched = Icite.fetch_citation_tree(ids=pmid, max_depth=int(argv["--max-depth"]))
 
-  with open(argv["--out"], 'w') as outfile:
-    json.dump({"length": len(fetched), "data": fetched}, outfile, indent=2)
+  results = {"length": len(fetched), "data": fetched}
+
+  if argv["--verbose"]:
+    with open(argv["--out"], 'w') as outfile:
+      json.dump(results, outfile, indent=2)
+    print(":saved %r" % argv["--out"])
+
+  else:
+    print(json.dump(results, sys.stdout, indent=2))
